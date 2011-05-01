d247792076b119b4de8fc18ff94b05717ef7cb65
rgrp <http://rufuspollock.org>
2011-05-01 13:10:41 +0100

    [model][s]: add persistence based on localstorage and add NoteList collection (+ tests for both).
    * Had to do a minor patch to backbone provided localstorage whose signature seems out of sync with main.

--- test/test_model.js
+++ test/test_model.js
@@ -1,12 +1,31 @@
 module("Model");
 
-test("Create note", function () {
+// use local test storge 
+HyperNotes.Model.Note.localStorage = new Store('test-note');
+
+test("Create note and note list", function () {
   var indata = {
     label: 'My New Note',
     body: '## Xyz',
     tags: ['abc', 'efg']
   };
-  var pkg = new HyperNote.Model.Note(indata);
+  var note = new HyperNotes.Model.Note(indata);
+  equals(note.get('label'), indata.label);
 
-  equals(pkg.get('label'), indata.label);
+  // test we can persist
+  note.save();
+  var outnote = new HyperNotes.Model.Note({id: note.id});
+  equals(outnote.get('label'), null);
+  outnote.fetch();
+  equals(outnote.get('label'), indata.label);
+
+  // test collection
+  indata2 = {
+    label: 'My New Note 2'
+  }
+  var notelist = new HyperNotes.Model.NoteList();
+  notelist.add([note]);
+  notelist.create(indata2);
+  equals(notelist.length, 2);
 });
+
