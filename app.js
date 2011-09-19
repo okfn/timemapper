38df50f486c27eca77d454755df95ad0ba6b662f
rgrp <rufus.pollock@okfn.org>
2011-09-19 12:46:19 +0100

    [api][m]: basics of the api (get/post for objects) ported to nodejs (no tests!).

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var dao = require('./dao.js');
 
 var app = module.exports = express.createServer();
+
+var indexName = 'hypernotes';
 
 // Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set("view engine", "html");
   app.register(".html", require("jqtpl").express);
+  app.set("jsonp callback");
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
@@ -15,18 +19,54 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler()); 
+  app.user(express.bodyParser());
 });
 
 // Routes
 app.get('/', function(req, res){
   res.render('index.html', {
   });
+});
+
+app.get('/api/v1/:objecttype/:id', function(req, res, next) {
+  // klass = getattr(logic, objecttype.capitalize())
+  // out = klass.get(req.params.id)
+  dao.get(indexName, req.params.objecttype, req.params.id)
+    .on('data', function(data) {
+      var out = JSON.parse(data);
+      if (!out.exists) {
+        // next(new Error('Cannot find ' + req.params.objecttype + ' with id ' + req.params.id));
+        var msg = 'Cannot find ' + req.params.objecttype + ' with id ' + req.params.id;
+        res.send(msg, 404);
+      } else {
+        res.send(out);
+      }
+    })
+    .exec();
+});
+
+app.post('/api/v1/:objecttype/:id?', function(req, res) {
+    var data = req.body;
+    if (req.params.id) {
+      data.id = req.params.id;
+    }
+    dao.upsert(indexName, req.params.objecttype, data, function(outData) {
+      var out = {
+          'status': 'ok'
+          , 'result': data
+      };
+      res.send(out)
+    });
 });
 
 app.listen(3000);
--- dao.js
+++ dao.js
@@ -11,9 +11,22 @@ function search(indexName, table, qryObj) {
   return esclient.search(indexName, table, qryObj)
 }
 
+function get(indexName, objectType, id) {
+  return esclient.get(indexName, objectType, id)
+}
+
+function upsert(indexName, objectType, data, callback) {
+  dao.esclient.index(indexName, objectType, data)
+    .on('data', function(data) {
+        var out = JSON.parse(data);
+        callback(out);
+    })
+    .exec()
+}
 
 module.exports = {
   esclient: esclient
   , search: search
+  , get: get
 };
 
