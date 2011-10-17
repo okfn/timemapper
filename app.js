10352c061026367b4300f140b7bb45cef1c3e655
rgrp <rufus.pollock@okfn.org>
2011-10-17 03:26:12 +0100

    [content][s]: create about page based on front page and simplify front page.

--- app.js
+++ app.js
@@ -94,6 +94,9 @@ app.get('/', function(req, res){
   res.render('index.html', {});
 });
 
+app.get('/about', function(req, res){
+  res.render('about.html', {});
+});
 
 // ======================================
 // User Accounts
