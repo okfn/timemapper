cda598b137ddade3b25d12f69d6c91537e7020ab
Rufus Pollock <rufus.pollock@okfn.org>
2012-01-02 01:55:54 +0000

    [refactor][xs]: move thread template into view.js.

--- public/js/template.js
+++ public/js/template.js
@@ -8,39 +8,6 @@ HyperNotes.Template = {
       <div id="mapcontainer"> \
         <div id="map"></div> \
       </div> \
-  ',
-
-  thread: ' \
-    <h1 class="title">${thread.title}</h1> \
-    <div class="thread-info"> \
-      <p class="description snippet">${thread.description}</p> \
-    </div> \
-    <div class="row"> \
-      <div class="span8 left-pane"> \
-        <div class="note-list"> \
-          <ul></ul> \
-        </div> \
-      </div> \
-      <div class="span8 right-pane"> \
-        {{if permissions.edit}} \
-        <div class="note quick-add"> \
-          <form> \
-            <label for="new-note">Quick Add</label> \
-            <div class="input"> \
-              <input \
-                name="new-note" \
-                placeholder="My note title #my-tag ^1st September 1939^ @New York@" \
-                type="text" /> \
-            </div> \
-            <span class="help-block"> \
-              Add note then enter to save. Use #... for tags, ^...^ for dates, @...@ for location \
-            </span> \
-          </form> \
-        </div> \
-        {{/if}} \
-        <div id="timemap"></div> \
-      </div> \
-    </div> \
-  ',
+  '
 };
 
--- public/js/view.js
+++ public/js/view.js
@@ -356,6 +356,38 @@ HyperNotes.View = function($) {
   });
 
   my.ThreadView = Backbone.View.extend({
+    template: ' \
+      <h1 class="title">${thread.title}</h1> \
+      <div class="thread-info"> \
+        <p class="description snippet">${thread.description}</p> \
+      </div> \
+      <div class="row"> \
+        <div class="span8 left-pane"> \
+          <div class="note-list"> \
+            <ul></ul> \
+          </div> \
+        </div> \
+        <div class="span8 right-pane"> \
+          {{if permissions.edit}} \
+          <div class="note quick-add"> \
+            <form> \
+              <label for="new-note">Quick Add</label> \
+              <div class="input"> \
+                <input \
+                  name="new-note" \
+                  placeholder="My note title #my-tag ^1st September 1939^ @New York@" \
+                  type="text" /> \
+              </div> \
+              <span class="help-block"> \
+                Add note then enter to save. Use #... for tags, ^...^ for dates, @...@ for location \
+              </span> \
+            </form> \
+          </div> \
+          {{/if}} \
+          <div id="timemap"></div> \
+        </div> \
+      </div> \
+    ',
     events: {
       "submit .note.quick-add form":  "createOnEnter",
     },
@@ -368,7 +400,7 @@ HyperNotes.View = function($) {
         thread: this.model.toJSON()
         , permissions: permissions
       };
-      var templated = $.tmpl(HyperNotes.Template.thread, tmplData);
+      var templated = $.tmpl(this.template, tmplData);
       $(this.el).html(templated);
       this.$notelist = this.el.find('.note-list');
       this.noteListView = new HyperNotes.View.NoteListView({
