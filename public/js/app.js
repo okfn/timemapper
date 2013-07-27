01e052d76cda8b6a7ab42b7ad86dc2237e458f2c
Rufus Pollock <rufus.pollock@okfn.org>
2013-07-27 02:35:13 +0100

    [import][m]: import current timeliner frontpage.
    * convert frontpage to inherit from base template in anticipation of other pages.

--- public/js/app.js
+++ public/js/app.js
@@ -1,71 +1,71 @@
-var HyperNotes = HyperNotes || {};
+jQuery(function($) {
+  $('form.js-load-url').submit(function(e) {
+    e.preventDefault();
+    var $form = $(e.target);
+    var source = $form.find('input[name="source"]').val();
+    var path = window.location.href.replace('/index.html', '');
+    if (path[path.length - 1] != '/') {
+      path += '/';
+    }
+    path +=  'view/?url=' + encodeURIComponent(source);
+    window.location.href = path;
+  });
 
-HyperNotes.Controller = function($) {
-  var my = {};
+  var $input = $('input[name="source"]');
+  $(".gdrive-import").click(function (e) {
+    e.preventDefault();
+    
+    var picker = new google.picker.PickerBuilder()
+      .disableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+      .addView(google.picker.ViewId.SPREADSHEETS)
+      .setCallback(pickerCallback)
+      .build();
+    picker.setVisible(true);
 
-  var Workspace = Backbone.Router.extend({
-    routes: {
-      "": "index",
-      "thread/view/:id": "threadView",
-      "dashboard": "dashboard"
-    },
-
-    initialize: function(environ) {
-      this.environ = environ;
-      this.environ.account = new HyperNotes.Model.Account(this.environ.config.account);
-    },
-
-    switchView: function(view) {
-      $('.page-view').hide();
-      $('#' + view + '-page').show();
-    },
-
-    index: function(query, page) {
-    },
-
-    threadView: function(thread) {
-      this.$thread = $('.thread.view');
-      var thread = new HyperNotes.Model.Thread(thread);
-      // hacky but best way to boot it i think
-      thread.fetch({
-        success: function() {
-          thread.updateNoteList();
-        }
-      });
-      my.threadView = new HyperNotes.View.ThreadView({
-        el: this.$thread,
-        model: thread
-        });
-    },
-
-    threadTimeMap: function(thread) {
-      var thread = new HyperNotes.Model.Thread(thread);
-      thread.updateNoteList();
-      my.timemap = new HyperNotes.View.TimeMapView({
-        el: $('.timemap'),
-        collection: thread.notes
-      });
-      my.timemap.render();
-    },
-
-    accountView: function() {
-      var thread = new HyperNotes.Model.Thread({
-        owner: this.environ.account.id
-      });
-      my.threadCreateView = new HyperNotes.View.ThreadCreate({
-        el: $('.thread-create-form') 
-        , model: thread
-      });
+    function pickerCallback(data) {
+      var url = 'nothing';
+      if (data[google.picker.Response.ACTION] === google.picker.Action.PICKED) {
+        var doc = data[google.picker.Response.DOCUMENTS][0];
+        url = doc[google.picker.Document.URL];
+        $input.val(url).trigger("change");
+      }
     }
   });
-  
-  my.initialize = function(config) {
-    var environ = HyperNotes.Environ(config);
-    HyperNotes.environ = environ;
-    my.workspace = new Workspace(environ);
-    Backbone.history.start()
-    return my.workspace;
-  };
 
-  return my;
-}(jQuery);
+  $input.change(function (e) {
+
+    // NB: setCustomValidity primes the error messages ready for form submission,
+    //     it doesn't show them immediately.
+    var url = recline.Backend.GDocs.getGDocsAPIUrls(e.target.value).spreadsheet;
+
+    $.ajax(url, {
+      type: "HEAD",
+      success: function (jqXHR) {
+        e.target.setCustomValidity("");
+      },
+      error: function (jqXHR, textStatus) {
+        var error;
+        if (jqXHR.status === 404) {
+          error = "That URL doesn't exist";
+        } else {
+          error = "We could not retrieve this URL. Have you published this spreadsheet?";
+        }
+        e.target.setCustomValidity(error);
+      }
+    });
+  });
+
+});
+
+// Backwards compatability (pre Feb 2013)
+// Redirect from old timeliner urls to new ones
+if (window.location.search) {
+  var path = window.location.href.replace(/\/index.html/, '');
+  path = path.replace(/\?.*/g, '');
+  if (path[path.length - 1] != '/') {
+    path += '/';
+  }
+  path +=  'view/' + window.location.search;
+  window.location.href = path;
+}
+
