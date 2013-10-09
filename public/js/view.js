okfn/timemapper

commit deee400020cfb68b0643dcce451f8029df003da5

[#66][s]: timemap viz utilizes dayfirst config introduced in last commit.

public/js/view.js
@@ -47,14 +47,19 @@ var TimelinerView = Backbone.View.extend({
     var self = this;
     // explicitly set width as otherwise Timeline does extends a bit too far (seems to use window width rather than width of actual div)
     // $el.width((this.el.width() - 45)/2.0);
+    var state = {};
+    if (VIZDATA && VIZDATA.tmconfig) {
+      state = _.extend({}, VIZDATA.tmconfig.timeline, {
+        nonUSDates: VIZDATA.tmconfig.dayfirst,
+        timelineJSOptions: _.extend({}, VIZDATA.tmconfig.timelineJSOptions, {
+          "hash_bookmark": true
+        })
+      });
+    }
     this.timeline = new recline.View.Timeline({
       model: this.model,
       el: this.$el.find('.timeline'),
-      state: {
-        timelineJSOptions: {
-          "hash_bookmark": true
-        }
-      }
+      state: state
     });
 
     // Timeline will sort the entries by timestamp, and we need the order to be

commit 4f597d99abc8fe2d74555aee8ab524329ce244f7

[#78,bugfix][s]: fix breakage of hash updating and hence timeline => map connection introduced in deee400020cfb68b0643dcce451f8029df003da5 - fixes #78.

public/js/view.js
@@ -48,14 +48,16 @@ var TimelinerView = Backbone.View.extend({
     // explicitly set width as otherwise Timeline does extends a bit too far (seems to use window width rather than width of actual div)
     // $el.width((this.el.width() - 45)/2.0);
     var state = {};
-    if (VIZDATA && VIZDATA.tmconfig) {
-      state = _.extend({}, VIZDATA.tmconfig.timeline, {
-        nonUSDates: VIZDATA.tmconfig.dayfirst,
-        timelineJSOptions: _.extend({}, VIZDATA.tmconfig.timelineJSOptions, {
-          "hash_bookmark": true
-        })
-      });
+    // backwards compat - going forward everything should have this
+    if (!VIZDATA.tmconfig) {
+      VIZDATA.tmconfig = {};
     }
+    state = _.extend({}, VIZDATA.tmconfig.timeline, {
+      nonUSDates: VIZDATA.tmconfig.dayfirst,
+      timelineJSOptions: _.extend({}, VIZDATA.tmconfig.timelineJSOptions, {
+        "hash_bookmark": true
+      })
+    });
     this.timeline = new recline.View.Timeline({
       model: this.model,
       el: this.$el.find('.timeline'),