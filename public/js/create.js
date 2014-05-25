7c1df696c9362b0ec2feafe6b04884c5aa1de3a3
Rufus Pollock <rufus.pollock@okfn.org>
2014-05-25 12:30:18 +0100

    [#138][m]: fix up google drive picker to work with new oauth requirements - fixes #138.

--- public/js/create.js
+++ public/js/create.js
@@ -6,20 +6,73 @@ jQuery(function($) {
   $(".gdrive-import").click(onGdriveImportClick);
   $input.change(onUrlChange);
   $('.js-demo-sheet').click(onDemoSheetClick);
+
+  // we will assume this always loads prior to anyone hitting the drive button
+  // (if not we would need to double-checker picker is loaded in drive stuff)
+  gapi.load('picker');
 });
 
+var getGDrivePickerOauth = function(callback) {
+  // Use the API Loader script to load google.picker and gapi.auth.
+  gapi.load('auth', {'callback': onAuthApiLoad});
+
+  // The Client ID obtained from the Google Developers Console.
+  var clientId = '1670524553-8saihco706hv7ilehume7g8a2fnj1ug5.apps.googleusercontent.com';
+  // Scope to use to access user's photos.
+  var scope = ['https://www.googleapis.com/auth/drive.readonly'];
+
+  function onAuthApiLoad() {
+    window.gapi.auth.authorize(
+      {
+        'client_id': clientId,
+        'scope': scope,
+        'immediate': false
+      },
+      handleAuthResult);
+  }
+
+  function handleAuthResult(authResult) {
+    if (authResult && !authResult.error) {
+      oauthToken = authResult.access_token;
+      callback(oauthToken);
+    } else {
+      alert('Error authorizing: ' + authResult.error);
+    }
+  }
+};
+
+var gDriveOauth = null;
 var onGdriveImportClick = function(e) {
-  var $input = $('.js-connect input[name="url"]');
   e.preventDefault();
-  
-  var picker = new google.picker.PickerBuilder()
-    .disableFeature(google.picker.Feature.MULTISELECT_ENABLED)
-    .addView(google.picker.ViewId.SPREADSHEETS)
-    .setCallback(pickerCallback)
-    .build();
-  picker.setVisible(true);
+
+  if (!gDriveOauth) {
+    // we call ourselves again when ready
+    getGDrivePickerOauth(function(oauth) {
+      gDriveOauth = oauth;
+      createPicker();
+    });
+    return;
+  } else {
+    createPicker();
+  }
+
+  function createPicker() {
+    // The API developer key obtained from the Google Developers Console.
+    var developerKey = 'AIzaSyBxklz1xd_L-whw-k2vR4pjWkgO6cqfLG0';
+
+    // Create and render a Picker object for picking user Photos.
+    var picker = new google.picker.PickerBuilder()
+      .disableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+      .addView(google.picker.ViewId.SPREADSHEETS)
+      .setOAuthToken(gDriveOauth)
+      .setDeveloperKey(developerKey)
+      .setCallback(pickerCallback)
+      .build();
+    picker.setVisible(true);
+  }
 
   function pickerCallback(data) {
+    var $input = $('.js-connect input[name="url"]');
     var url = 'nothing';
     if (data[google.picker.Response.ACTION] === google.picker.Action.PICKED) {
       var doc = data[google.picker.Response.DOCUMENTS][0];
