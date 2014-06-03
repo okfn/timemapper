cb63e7922e93572b0edda6f4344fd562511ea81d
陳信屹 <ossug.hychen@gmail.com>
2014-06-04 00:00:03 +0800

    use makeDefaultConfg to generate default settings of nconf.

--- test/config.test.js
+++ test/config.test.js
@@ -14,19 +14,20 @@ describe('config', function() {
     assert.equal(config.get('express:port'), 5000);
     assert.equal(config.get('twitter:key'), undefined);
     assert.equal(config.get('twitter:secret'), undefined);
-    assert.equal(config.get('twitter:url'), 
+    assert.equal(config.get('twitter:callback'), 
       "http://timemapper.okfnlabs.org/account/auth/twitter/callback");
     assert.equal(config.get('s3:key'), undefined);
     assert.equal(config.get('s3:secret'), undefined);
     assert.equal(config.get('s3:bucket'), "timemapper-data.okfnlabs.org");    
-    assert.equal(config.get('test:testing'), "false");
+    assert.equal(config.get('test:testing'), false);
     assert.equal(config.get('test:user'), 'tester');
     done();
   });
   it('should use environment variable if it is given. .', function(done) {
-    process.env['DB_PATH'] = 'x'
-    process.env['BACKEND'] = 'x'
-    process.env['PORT'] = 'x'
+    process.env['DATABASE_PATH'] = 'x'
+    process.env['DATABASE_BACKEND'] = 'x'
+    process.env['EXPRESS_PORT'] = 'x'
+    process.env['EXPRESS_SECRET'] = 'x'
     process.env['TWITTER_KEY'] = 'x'
     process.env['TWITTER_SECRET'] = 'x'
     process.env['TWITTER_CALLBACK'] = 'x'
@@ -37,10 +38,11 @@ describe('config', function() {
     config = require('../lib/config');
     assert.equal(config.get('database:path'), 'x');
     assert.equal(config.get('database:backend'), 'x');
+    assert.equal(config.get('express:secret'), 'x');
     assert.equal(config.get('express:port'), 'x');
     assert.equal(config.get('twitter:key'), 'x');
     assert.equal(config.get('twitter:secret'), 'x');
-    assert.equal(config.get('twitter:url'), 'x');
+    assert.equal(config.get('twitter:callback'), 'x');
     assert.equal(config.get('s3:key'), 'x');
     assert.equal(config.get('s3:secret'), 'x');
     assert.equal(config.get('s3:bucket'), 'x');
